bd59fe542dbb3e495af73b0f876b5b11240de550
Kevin <kh1990@users.noreply.github.com>
2018-03-29 13:23:23 -0700

    Added comments

--- src/actions/bus_actions.js
+++ src/actions/bus_actions.js
@@ -41,7 +41,17 @@ const updateBusHelper = (dispatch, updateTime) => {
 //Fetch status along with the bus data
 //since we only need this once at the begining, we only need to call it once
 //it may seem excessive to separate the two, but this way it avoids unecessary
-//updates of the db
+//updates of the db. Since once you have multiple users viewing the website
+//simulatneously, you do not want every instance to be updating the database with
+//the update tag. This update code would be normally simply be a intervaled request
+//on a normal server, however, one of firebase cloud functions limitations is that
+//it must be triggered by something, either db change or externally. You can use a cron job
+//to trigger the get bus cloud function to keep semi up to date data.
+//TBH, this status function would not be necessary if the web app reaches scale, since
+//every time someone visits the website, the firebase database would be updated (if it is
+//within certain seconds of the update time) so the db would
+//realistically speaking always have real time data. 
+
 export const getStatus = () => (dispatch) => {
   const rootRef = fire.database().ref('/translink/status/');
 
